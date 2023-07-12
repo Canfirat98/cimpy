be913757da2ae313fae37b1b3a21a14ba842d3a0
Canfirat98 <can.yaruk@rwth-aachen.de>
2023-07-12 16:59:41 +0200

    CIMpy to DPSim Version 1.1 - Connection Nodes have to be done
    Signed-off-by: Canfirat98 <can.yaruk@rwth-aachen.de>

--- cimpy/CIMpyToDPsim.py
+++ cimpy/CIMpyToDPsim.py
@@ -1,41 +1,81 @@
 import sys
 sys.path.insert(0,'/home/mmo-cya/dpsim/build')
 import dpsimpy
+from enum import Enum
+import numpy
 
 #Nodes
-Nodes = []
-PiLines = []
-Slacks = []
+Nodes = dict()
+Components = dict()
 
-def CIMpyToDPsim(CIM_network):
 
-    sim_name_pf = "SP_ReducedOrderSG_SMIB_" + "PF_"
-    sim_pf = dpsimpy.Simulation(sim_name_pf, dpsimpy.LogLevel.debug)
+# define dpsimpy domains
+class Domain(Enum):
+    PF = 1
+    SP = 2
+    DP = 3
+    EMT = 4
+
+frequency = 60
+
+# Domains hinzufügen
+def CIMpyToDPsim(CIM_network, domain):
 
     res = CIM_network["topology"]
-    gnd = dpsimpy.sp.SimNode.gnd
+
+    # get the correct module to be used (dpsimpy.sp, dpsimpy.dp or dpsimpy.emt) 
+    dpsimpy_components = None
+    if (domain == 1):
+        dpsimpy_components = dpsimpy.sp.ph1
+    elif (domain == 2):
+        dpsimpy_components = dpsimpy.sp.ph1
+    elif (domain == 3):
+        dpsimpy_components = dpsimpy.dp.ph1
+    elif (domain == 4):
+        dpsimpy_components = dpsimpy.emt.ph3
+    else:
+        raise Exception('ERROR: domain {} is not supported in dpsimpy'.format(domain))
+
     
     for i in res:
-        if 'TopologicalNode' in str(type(res[i])):
-            ### Nodes
-            n1 = dpsimpy.sp.SimNode(res[i].mRID, dpsimpy.PhaseType.Single)
-            Nodes.append(n1)
-        
         ### Components 
-
         if 'ACLineSegment' in str(type(res[i])):
-            pi_line = dpsimpy.sp.ph1.PiLine(res[i].mRID, dpsimpy.LogLevel.debug)
+            # PiLine
+            pi_line = dpsimpy_components.PiLine(res[i].mRID, dpsimpy.LogLevel.debug)
             pi_line.set_parameters(R= res[i].r,
-                                #L=line_inductance,
-                                #C=line_capacitance,
+                                L=res[i].x/(2*numpy.pi*frequency),
+                                C=res[i].bch/(2*numpy.pi*frequency),
                                 G=res[i].gch)
-            PiLines.append(pi_line)
+            Components[pi_line.name()] = pi_line
 
-        if 'ExternalNetworkInjection' in str(type(res[i])):
+        elif 'ExternalNetworkInjection' in str(type(res[i])):
             # Slack
-            slack = dpsimpy.sp.ph1.NetworkInjection(res[i].mRID, dpsimpy.LogLevel.debug)
+            slack = dpsimpy_components.NetworkInjection(res[i].mRID, dpsimpy.LogLevel.debug)
             #slack.set_parameters(V_ref=nominal_voltage_mv)
-            Slacks.append(slack)
+            Components[slack.name()] = slack
+
+        elif 'SynchronousMachineTimeConstantReactance' in str(type(res[i])):
+            # Synchron Generator
+            gen = dpsimpy_components.SynchronGenerator3OrderVBR(res[i].mRID, dpsimpy.LogLevel.debug)
+            gen.set_operational_parameters_per_unit(H=res[i].inertia, Ld=res[i].xDirectSync, Lq=res[i].xQuadSync,
+                                                    Ld_t=res[i].xDirectTrans, Td0_t=res[i].tpdo)
+            Components[gen.name()] = gen
+
+    for j in res:
+        ### Nodes
+        if 'TopologicalNode' in str(type(res[j])): 
+            n1 = dpsimpy.sp.SimNode(res[j].mRID, dpsimpy.PhaseType.Single)
+            Nodes[n1.name()] = n1
+            Terminals = res[j].Terminal
+            for terminal in Terminals:
+                component_mRID = terminal.ConductingEquipment.mRID
+                # Hier auf ACLineSegment achten, mehrere Nodes möglich !
+                Components[component_mRID].connect([n1])
 
         
+    
+    node_list = list(Nodes.values())
+    component_list =list(Components.values())
+    system = dpsimpy.SystemTopology(frequency, node_list, component_list)
 
+    return system
